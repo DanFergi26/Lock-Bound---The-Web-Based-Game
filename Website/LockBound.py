6ed92e07e6fd9a3c5e3efb82954f04330c114f77
SweetPrince747 <40534169@live.napier.ac.uk>
2025-02-17 17:49:44 +0000

    update

--- Website/LockBound.py
+++ Website/LockBound.py
@@ -1,36 +1,56 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, request, redirect, url_for, flash, session, render_template
 import sqlite3
-import flask_sqlalchemy
-import flask_bcrypt 
-# might want to use flash to give incorrect password messages 
+from flask_sqlalchemy import SQLAlchemy # remember to download flask-sqlalchemy on server of deployment
+from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Set a secret key for sessions
 
-DB_FILE = 'users.db'
-CLARK_FILE = 'clark.db'
-PROFILE_PIC_FOLDER = 'profile_pics'
-os.makedirs(PROFILE_PIC_FOLDER, exist_ok=True)
+db = 'users.db'
+#os.makedirs(PROFILE_PIC_FOLDER, exist_ok=True)
 
-# Initialize database
-def init_db():
-    with sqlite3.connect(DB_FILE) as conn:
-        cursor = conn.cursor()
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS users (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                username TEXT UNIQUE NOT NULL,
-                password TEXT NOT NULL,
-                surname TEXT NOT NULL,
-                forename TEXT NOT NULL,
-                email TEXT UNIQUE NOT NULL,
-                profile_pic TEXT
-            )
-        ''')
-        conn.commit()
+# secret key used for session management
+app.config["SECRET_KEY"] = "gb2576eetc445"
 
-init_db()
+# database code for the create account and login page
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(os.path.dirname(__file__), "instance", "users.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+#initialiseas database
+db = SQLAlchemy(app) 
+
+# create the database file if it doesn't exist
+with app.app_context():
+    if not os.path.exists(os.path.join(os.path.dirname(__file__), "instance", "users.db")):
+        db.create_all()
+
+# decleration used for database table security/encryption
+bcrypt = Bcrypt(app)
+
+# creates a class within databse called User which contains the user id, username, password,
+# and a timestamp (created_at) attribute
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(8), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    # uses the bcrypt library   
+    def set_password(self, password):
+        # generates a hashed version of the password using bcrypt, 
+        # then stores it in the password attribute of user table within the database 
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+        
+    def check_password(self, password):
+        # checks if the password matches the hashed password stored in the database
+        # returns true or false
+        return bcrypt.check_password_hash(self.password, password)
+    
+
+
+
+
 
 @app.route('/')
 def home():
@@ -69,6 +89,7 @@ def signup_form():
         surname = request.form['surname']
         forename = request.form['forename']
         email = request.form['email']
+        birth = request.form['birth']
 
         with sqlite3.connect(DB_FILE) as conn:
             cursor = conn.cursor()
@@ -80,9 +101,9 @@ def signup_form():
 
             # Insert new user
             cursor.execute('''
-                INSERT INTO users (username, password, surname, forename, email, profile_pic)
+                INSERT INTO users (username, password, surname, forename, email, birth, profile_pic)
                 VALUES (?, ?, ?, ?, ?, ?, ?)
-            ''', (username, password, surname, forename, email, profile_pic_path))
+            ''', (username, password, surname, forename, email, birth, profile_pic_path))
             conn.commit()
 
         # Redirect to home page after signup
@@ -107,10 +128,15 @@ def account(username):
         'surname': user[3],
         'forename': user[4],
         'email': user[5],
-        'profile_pic': user[6]
+        'birth': user[6],
+        'profile_pic': user[7]
     }
 
     return render_template('account.html', user_info=user_info)
+
+# used for removing the database file and dropping all the tables 
+#if not os.path.exists(os.path.join(os.path.dirname(__file__), "instance", "users.db")):
+#        db.drop_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
